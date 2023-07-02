ff830494524a4b78516234f1ebb0c139a3692d6b
Lukas1v <51420170+Lukas1v@users.noreply.github.com>
2023-07-02 19:36:47 +0000

    devcontainer

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+
+DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends apt-utils dialog dnsutils httpie wget unzip curl jq
+DEBIAN_FRONTEND=dialog
+
+#install python packages
+pip install --upgrade pip
+pip install -r requirements.txt
+
+#install Terraform
+getLatestVersion() {
+LATEST_ARR=($(wget -q -O- https://api.github.com/repos/hashicorp/terraform/releases 2> /dev/null | awk '/tag_name/ {print $2}' | cut -d '"' -f 2 | cut -d 'v' -f 2))
+for ver in "${LATEST_ARR[@]}"; do
+  if [[ ! $ver =~ beta ]] && [[ ! $ver =~ rc ]] && [[ ! $ver =~ alpha ]]; then
+    LATEST="$ver"
+    break
+  fi
+done
+echo -n "$LATEST"
+}
+
+VERSION=$(getLatestVersion)
+cd ~
+echo "installing Terraform version $VERSION"
+wget "https://releases.hashicorp.com/terraform/"$VERSION"/terraform_"$VERSION"_linux_amd64.zip"
+unzip "terraform_"$VERSION"_linux_amd64.zip"
+sudo install terraform /usr/local/bin/
