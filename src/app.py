85ca106b210df8f4928538f3f81426669826ae75
Lukas1v <51420170+Lukas1v@users.noreply.github.com>
2023-07-14 19:36:42 +0000

    app.py refactored OO

--- src/app.py
+++ src/app.py
@@ -9,111 +9,131 @@ openai.api_type = "azure"
 openai.api_base = os.getenv("OPENAI_ENDPOINT") 
 openai.api_version = "2023-05-15"
 openai.api_key = os.getenv("OPENAI_API_KEY")
-engine='custom-chatgpt-model'
-
-# Initialise session state variables
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."}
-    ]
-if 'model_name' not in st.session_state:
-    st.session_state['model_name'] = []
-if 'cost' not in st.session_state:
-    st.session_state['cost'] = []
-if 'total_tokens' not in st.session_state:
-    st.session_state['total_tokens'] = []
-if 'total_cost' not in st.session_state:
-    st.session_state['total_cost'] = 0.0
-
-#page title
-st.set_page_config(page_title="LVE Chatbot", page_icon=":robot_face:")
 
 
-## Sidebar
-# let user choose model, show total cost of current conversation, and let user clear the current conversation
-st.sidebar.title("Sidebar")
-model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4 (under development)"))
-counter_placeholder = st.sidebar.empty()
-counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
-clear_button = st.sidebar.button("Clear Conversation", key="clear")
+class chatBot():
+    def __init__(self):
+        #Azure openai engine
+        self.engine_v35='custom-chatgpt-model'
+        self.engine_v40='custom-chatgpt-model'
+        
+        # Initialise streamlit and set page title
+        st.set_page_config(page_title="LVE Chatbot", page_icon=":robot_face:")
 
-# Map model names to Deployment id's, only 3.5 is available on Azure
-if model_name == "GPT-3.5":
-    engine = engine
-else:
-    engine = engine
+        # Initialise session state variables
+        if 'generated' not in st.session_state:
+            st.session_state['generated'] = []
+        if 'past' not in st.session_state:
+            st.session_state['past'] = []
+        if 'messages' not in st.session_state:
+            st.session_state['messages'] = [
+                {"role": "system", "content": "You are a helpful assistant."}
+            ]
+        if 'model_name' not in st.session_state:
+            st.session_state['model_name'] = []
+        if 'cost' not in st.session_state:
+            st.session_state['cost'] = []
+        if 'total_tokens' not in st.session_state:
+            st.session_state['total_tokens'] = []
+        if 'total_cost' not in st.session_state:
+            st.session_state['total_cost'] = 0.0
+        # self.clear_state()
 
-# reset everything
-if clear_button:
-    st.session_state['generated'] = []
-    st.session_state['past'] = []
-    st.session_state['messages'] = [
-        {"role": "system", "content": "You are a helpful assistant."}
-    ]
-    st.session_state['number_tokens'] = []
-    st.session_state['model_name'] = []
-    st.session_state['cost'] = []
-    st.session_state['total_cost'] = 0.0
-    st.session_state['total_tokens'] = []
-    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+        # Setting  header and sidebar static properties
+        st.markdown("<h1 style='text-align: center;'>Custom Chatbot by LVE Consulting </h1>", unsafe_allow_html=True)
+        st.sidebar.title("Sidebar")
+        self.counter_placeholder = st.sidebar.empty()
 
+    def clear_state(self):
+        st.session_state['generated'] = []
+        st.session_state['past'] = []
+        st.session_state['messages'] = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
+        st.session_state['number_tokens'] = []
+        st.session_state['model_name'] = []
+        st.session_state['cost'] = []
+        st.session_state['total_cost'] = 0.0
+        st.session_state['total_tokens'] = []
 
-## Main page
-# Setting  header
-st.markdown("<h1 style='text-align: center;'>Custom Chatbot by LVE Consulting </h1>", unsafe_allow_html=True)
-
-# generate a response
-def generate_response(prompt):
-    st.session_state['messages'].append({"role": "user", "content": prompt})
-
-    completion = openai.ChatCompletion.create(
-        engine=engine,
-        messages=st.session_state['messages']
-    )
-    response = completion.choices[0].message.content
-    st.session_state['messages'].append({"role": "assistant", "content": response})
-
-    # print(st.session_state['messages'])
-    total_tokens = completion.usage.total_tokens
-    prompt_tokens = completion.usage.prompt_tokens
-    completion_tokens = completion.usage.completion_tokens
-    return response, total_tokens, prompt_tokens, completion_tokens
-
-
-# container for chat history and text box
-response_container = st.container()
-container = st.container()
-
-with container:
-    with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_area("You:", key='input', height=100)
-        submit_button = st.form_submit_button(label='Send')
-
-    if submit_button and user_input:
-        output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
-        st.session_state['past'].append(user_input)
-        st.session_state['generated'].append(output)
-        st.session_state['model_name'].append(model_name)
-        st.session_state['total_tokens'].append(total_tokens)
-
-        # from https://openai.com/pricing#language-models
+    def set_model(self, model_name):
         if model_name == "GPT-3.5":
-            cost = total_tokens * 0.002 / 1000
+            engine = self.engine_v35
         else:
-            cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+            engine = self.engine_v35 
+        return engine
+    
+    def clear(self):
+        self.clear_state
+        self.counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
 
-        st.session_state['cost'].append(cost)
-        st.session_state['total_cost'] += cost
+    def run(self):
+        # ## Sidebar
+        # let user choose model, show total cost of current conversation, and let user clear the current conversation     
+        self.counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+        clear_button = st.sidebar.button("Clear Conversation", key="clear")
 
-if st.session_state['generated']:
-    with response_container:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
-            st.write(
-                f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
-            counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+        # Map model names to Deployment id's, only 3.5 is available on Azure
+        model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4 (under development)"))
+        engine = self.set_model(model_name)
+
+        # reset everything
+        if clear_button:
+            self.clear()
+
+        ## generate a response
+        def generate_response(prompt):
+            st.session_state['messages'].append({"role": "user", "content": prompt})
+
+            completion = openai.ChatCompletion.create(
+                engine=engine,
+                messages=st.session_state['messages']
+            )
+            response = completion.choices[0].message.content
+            st.session_state['messages'].append({"role": "assistant", "content": response})
+
+            # print(st.session_state['messages'])
+            total_tokens = completion.usage.total_tokens
+            prompt_tokens = completion.usage.prompt_tokens
+            completion_tokens = completion.usage.completion_tokens
+            return response, total_tokens, prompt_tokens, completion_tokens
+
+
+        # container for chat history and text box
+        response_container = st.container()
+        container = st.container()
+
+        with container:
+            with st.form(key='my_form', clear_on_submit=True):
+                user_input = st.text_area("You:", key='input', height=100)
+                submit_button = st.form_submit_button(label='Send')
+
+            if submit_button and user_input:
+                output, total_tokens, prompt_tokens, completion_tokens = generate_response(user_input)
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
+                st.session_state['model_name'].append(model_name)
+                st.session_state['total_tokens'].append(total_tokens)
+
+                # from https://openai.com/pricing#language-models
+                if model_name == "GPT-3.5":
+                    cost = total_tokens * 0.002 / 1000
+                else:
+                    cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+
+                st.session_state['cost'].append(cost)
+                st.session_state['total_cost'] += cost
+
+        if st.session_state['generated']:
+            with response_container:
+                for i in range(len(st.session_state['generated'])):
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+                    message(st.session_state["generated"][i], key=str(i))
+                    st.write(
+                        f"Model used: {st.session_state['model_name'][i]}; Number of tokens: {st.session_state['total_tokens'][i]}; Cost: ${st.session_state['cost'][i]:.5f}")
+                    self.counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+
+
+if __name__ == '__main__':
+    cb = chatBot()
+    cb.run()
